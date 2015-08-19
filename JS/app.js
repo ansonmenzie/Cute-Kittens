c3a605236fe477a656272df00ad71e223154d779
Anson Menzie <anson.menzie@gmail.com>
2015-08-18 22:57:14 -0700

    Still having some functionality issues.  Got some jQuery in

--- JS/app.js
+++ JS/app.js
@@ -1,108 +1,109 @@
 'use strict';
 
-var photoArray = [];
+$(document).ready(function() {
 
-var Photo = function(fileLocation) {
-  //constructor
+  var photoArray = [];
 
-  this.path = fileLocation;
-  this.votes = 0;
-  this.index = photoArray;//where it lives in  photoArray?
+  var Photo = function(fileLocation) {
+    //constructor
 
-};
+    this.path = fileLocation;
+    this.votes = 0;
 
-photoArray.push(new Photo('img/kitten1.jpg'));
-photoArray.push(new Photo('img/kitten2.jpg'));
-photoArray.push(new Photo('img/kitten3.jpg'));
-photoArray.push(new Photo('img/kitten4.jpg'));
-photoArray.push(new Photo('img/kitten5.jpg'));
-photoArray.push(new Photo('img/kitten6.jpg'));
-photoArray.push(new Photo('img/kitten7.jpg'));
-photoArray.push(new Photo('img/kitten8.jpg'));
-photoArray.push(new Photo('img/kitten9.jpg'));
-photoArray.push(new Photo('img/kitten10.jpg'));
-photoArray.push(new Photo('img/kitten11.jpg'));
-photoArray.push(new Photo('img/kitten12.jpg'));
-photoArray.push(new Photo('img/kitten13.jpg'));
-photoArray.push(new Photo('img/kitten14.jpg'));
+  };
 
-var leftVotes;
-var rightVotes;
+  photoArray.push(new Photo('img/kitten1.jpg'));
+  photoArray.push(new Photo('img/kitten2.jpg'));
+  photoArray.push(new Photo('img/kitten3.jpg'));
+  photoArray.push(new Photo('img/kitten4.jpg'));
+  photoArray.push(new Photo('img/kitten5.jpg'));
+  photoArray.push(new Photo('img/kitten6.jpg'));
+  photoArray.push(new Photo('img/kitten7.jpg'));
+  photoArray.push(new Photo('img/kitten8.jpg'));
+  photoArray.push(new Photo('img/kitten9.jpg'));
+  photoArray.push(new Photo('img/kitten10.jpg'));
+  photoArray.push(new Photo('img/kitten11.jpg'));
+  photoArray.push(new Photo('img/kitten12.jpg'));
+  photoArray.push(new Photo('img/kitten13.jpg'));
+  photoArray.push(new Photo('img/kitten14.jpg'));
 
-var ctx = document.getElementById("myChart").getContext("2d");
-console.dir(Chart)
-var myNewChart = new Chart(ctx).Doughnut([{value: leftVotes, color: 'red'}, {value: rightVotes, color: 'green'}]);
-console.dir(Chart);
+  var leftVotes;
+  var rightVotes;
+
+  var Tracker = function(leftPhoto, rightPhoto) {
+    this.leftPhoto = leftPhoto;
+    this.rightPhoto = rightPhoto;
+  };
+  var tracker = new Tracker();
 
 
-var Tracker = function() {
-this.leftPhoto = '';
-this.rightPhoto = '';
-};
-var tracker = new Tracker();
+  Tracker.prototype.getRandomInt = function() {
+    var random = (Math.floor(Math.random() * 14) );
+    return random;
+  //generate a random number to select an image from img folder
+    // photoArray[random].displayPhotos();
+  };
 
-Photo.prototype.highlight = function() {
-//highlight the photo after it is clicked
-//Does this need JS functionality? Can it be done with just css????
-};
+  Tracker.prototype.displayPhotos = function() {
+    //display the randomly selected photo
+    // prevent picking same photo twice
+    this.leftPhoto = photoArray[this.getRandomInt()];
+    this.rightPhoto = photoArray[this.getRandomInt()];
+    while (this.leftPhoto == this.rightPhoto) {
+      this.rightPhoto = photoArray[this.getRandomInt()];
+    }
 
-Tracker.prototype.waitingForVote = function() {
-  left.addEventListener("click", function(e) {
-  var targetSrc = e.target.src.slice(44, 100)
-    console.log('left was clicked')
-    photoArray.forEach(function(val) {
-      if(val.path === targetSrc) {
-        val.votes ++;
-      }
-      leftVotes = val.votes;
-    })
-    // this.displayPhotos();
+    var left = $('#left')[0];
+    var right = $('#right')[0];
+    // var left = document.getElementById("left");
+    // var right = document.getElementById("right");
+    var leftContent = '<img src = "' + this.leftPhoto.path + '" />';
+    var rightContent = '<img src = "' + this.rightPhoto.path + '" />';
+    left.innerHTML = leftContent;
+    right.innerHTML = rightContent;
+    highlight();
+
+  };
+
+   var ctx = $("#myChart").get(0).getContext("2d");
+  var myDoughnutChart = new Chart(ctx).Doughnut(data);
+  var data = [
+    {
+        value: 1,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Right Kitty"
+    },
+    {
+        value: 1,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "left Kitty"
+    }
+  ]
+  var myDoughnutChart = new Chart(ctx).Doughnut(data);
+
+  Tracker.prototype.addVote = function() {
+    var index = photoArray[this.getRandomInt()];
+    photoArray[index]++;
+  };
+
+  $('button').click(function(){
+    tracker.addVote($('.highlight').attr('src'));
+    $('button').css({'visibility':'hidden'});
+    tracker.displayPhotos();
   });
-  right.addEventListener("click", function(e) {
-  var targetSrc = e.target.src.slice(44, 100)
-    console.log('right was clicked')
-    photoArray.forEach(function(val) {
-      if(val.path === targetSrc) {
-        val.votes ++;
-      }
-      rightVotes = val.votes;
-    })
-    // this.displayPhotos();
-  });
-  //receive the click
-  //increment the vote count
-  //event listener on each photo
-  //hightlight()
-  //drawTheChart
-  //giveUserOptionToVoteAgain()?
-};
-//receiveVote()
 
-Tracker.prototype.getRandomInt = function() {
-  return(Math.floor(Math.random() * 14) );
-  // console.log(random)
-//generate a random number to select an image from img folder
-  // photoArray[random].displayPhotos();
-};
+  var highlight = function(){
+    $('div').click(function(){
+      $('button').css({'visibility':'visible'});
+      $('.highlight').removeClass('highlight');
+      $(this).toggleClass('highlight');
+    });
 
+  tracker.displayPhotos();
+  tracker.addVote();
+  };
 
-Tracker.prototype.displayPhotos = function() {
-  //display the randomly selected photo
-  // prevent picking same photo twice
-  this.leftPhoto = photoArray[this.getRandomInt()];
-  this.rightPhoto = photoArray[this.getRandomInt()];
-  while (this.leftPhoto == this.rightPhoto) {
-    this.rightPhoto = this.getRandomInt();
-  }
-  var left = document.getElementById("left");
-  left.innerHTML = '<img src = "img/kitten10.jpg">'
-  var right = document.getElementById("right");
-  right.innerHTML = '<img src = "img/kitten13.jpg">'
-};
+});
 
-tracker.displayPhotos();
-tracker.waitingForVote();
-
-
-
-//some document.getElementById variables to access and manipulate the document
