699e55bed5459f3d7e1ea45efe3da0f4f99a5b5c
Anson Menzie <anson.menzie@gmail.com>
2015-08-21 11:19:58 -0700

    CSS was broken.  FIxed it.

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-//app.get('/secret', function(request, response) {
+app.get('/secret', function(request, response) {
   response.send('Look no further feller, you found him!');
 })
 
